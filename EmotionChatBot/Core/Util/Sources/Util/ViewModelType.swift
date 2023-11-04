cab21cb0ebf187a5cf2acbca429ed92f016dac10
chansooo <kcs05008@gmail.com>
2023-11-04 17:27:17 +0900

    feat: ViewModelType 구현

---
+++ EmotionChatBot/Core/Util/Sources/Util/ViewModelType.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModelType.swift
+//  
+//
+//  Created by kimchansoo on 2023/11/04.
+//
+
+import RxSwift
+
+public protocol ViewModelType: AnyObject {
+    
+    associatedtype Input
+    associatedtype Output
+    
+    // MARK: - Properties
+    var disposeBag: DisposeBag { get }
+    
+    // MARK: - Methods
+    func transform(input: Input) -> Output
+}
