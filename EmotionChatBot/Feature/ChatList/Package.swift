f259a1a073c74983902fa03189418a492d658b94
chansooo <kcs05008@gmail.com>
2023-11-04 16:36:41 +0900

    feat: ChatList dependency 추가

--- EmotionChatBot/Feature/ChatList/Package.swift
+++ EmotionChatBot/Feature/ChatList/Package.swift
@@ -6,23 +6,40 @@ import PackageDescription
 let package = Package(
     name: "ChatList",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ChatList",
             targets: ["ChatList"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/Quick/Nimble",
+            .upToNextMajor(from: "12.0.0")
+        ),
+        .package(
+            url: "https://github.com/Quick/Quick",
+            .upToNextMajor(from: "6.0.0")
+        ),
+        .package(
+            url: "https://github.com/ReactiveX/RxSwift",
+            .upToNextMajor(from: "6.0.0")
+        ),
+//        .package(path: "./ChallengeConfirmScene")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
             name: "ChatList",
-            dependencies: []),
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RxRelay", package: "RxSwift"),
+            ]),
         .testTarget(
             name: "ChatListTests",
-            dependencies: ["ChatList"]),
+            dependencies: [
+                "ChatList",
+                .product(name: "Nimble", package: "Nimble"),
+                .product(name: "Quick", package: "Quick"),
+            ]),
     ]
 )
