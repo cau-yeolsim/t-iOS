a61a4d48d67fd6e3b7488aa2a56e69266b38ce2e
chansooo <kcs05008@gmail.com>
2023-11-05 15:50:30 +0900

    move: 폴더링 이동

--- EmotionChatBot/Core/Util/Sources/Util/Base/ViewModelType.swift
+++ EmotionChatBot/Core/Util/Sources/Util/Base/ViewModelType.swift
@@ -1,12 +1,15 @@
 //
 //  ViewModelType.swift
-//  
+//
 //
 //  Created by kimchansoo on 2023/11/04.
 //
 
 import RxSwift
 
+import Combine
+
+@MainActor
 public protocol ViewModelType: AnyObject {
     
     associatedtype Input
@@ -14,6 +17,7 @@ public protocol ViewModelType: AnyObject {
     
     // MARK: - Properties
     var disposeBag: DisposeBag { get }
+//    var cancellable: Set<AnyCancellable> { get }
     
     // MARK: - Methods
     func transform(input: Input) -> Output
