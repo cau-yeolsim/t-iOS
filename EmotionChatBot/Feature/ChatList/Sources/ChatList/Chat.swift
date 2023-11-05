84e47b068c978468bc1245577abd75be2bfda262
chansooo <kcs05008@gmail.com>
2023-11-05 15:52:23 +0900

    feat: Chat 모델 구현

--- EmotionChatBot/Feature/ChatList/Sources/ChatList/Chat.swift
+++ EmotionChatBot/Feature/ChatList/Sources/ChatList/Chat.swift
@@ -1,12 +1,25 @@
 //
-//  File.swift
+//  Chat.swift
 //
 //
-//  Created by kimchansoo on 2023/11/04.
+//  Created by kimchansoo on 2023/11/05.
 //
 
 import Foundation
 
 public struct Chat {
-    let preview: String
+    
+    let chatID: String
+    let message: String
+    let isMyMessage: Bool
+    
+    public init(
+        chatID: String,
+        message: String,
+        isMyMessage: Bool
+    ) {
+        self.chatID = chatID
+        self.message = message
+        self.isMyMessage = isMyMessage
+    }
 }
