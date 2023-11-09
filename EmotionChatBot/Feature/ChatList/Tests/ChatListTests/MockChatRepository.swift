f2124593a637cd8fd4fb2ad6a2bce065e8b48cb2
chansooo <kcs05008@gmail.com>
2023-11-09 23:02:38 +0900

    fix: MockChatRepository 동적으로 값 변경할 수 있도록 수정

--- EmotionChatBot/Feature/ChatList/Tests/ChatListTests/MockChatRepository.swift
+++ EmotionChatBot/Feature/ChatList/Tests/ChatListTests/MockChatRepository.swift
@@ -14,56 +14,18 @@ import RxSwift
 final class MockChatRepository: ChatRepository {
     
     // MARK: - Properties
+    var chatDetailResult: [Chat] = []
+    var chatRoomResult: [ChatRoom] = []
     
     // MARK: - Initializers
     
     // MARK: - Methods
     func fetchChatRoomList() -> Observable<[ChatRoom]> {
-        return Observable.create { observer in
-            observer.onNext([
-                ChatRoom(
-                    chatRoomId: "1",
-                    previewImgString: "",
-                    previewMessage: "안녕하세요?",
-                    nickname: "김찬수"
-                ),
-                ChatRoom(
-                    chatRoomId: "2",
-                    previewImgString: "",
-                    previewMessage: "안녕하세요?222",
-                    nickname: "문상헌"
-                ),
-                ChatRoom(
-                    chatRoomId: "3",
-                    previewImgString: "",
-                    previewMessage: "안녕하세요??333",
-                    nickname: "양석환"
-                ),
-            ])
-            return Disposables.create()
-        }
+        return .just(self.chatRoomResult)
     }
     
     func fetchChatDetail(chatId: String) -> Observable<[Chat]> {
-        return Observable.create { oberver in
-            oberver.onNext([
-                Chat(
-                    chatID: "1",
-                    message: "hi",
-                    isMyMessage: true
-                ),
-                Chat(
-                    chatID: "2",
-                    message: "hihi",
-                    isMyMessage: false
-                ),
-                Chat(
-                    chatID: "3",
-                    message: "hihihi",
-                    isMyMessage: true
-                ),
-            ])
-            return Disposables.create()
-        }
+        return .just(chatDetailResult)
     }
 }
+
