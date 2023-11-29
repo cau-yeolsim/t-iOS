c67123dae90426b5c56a8e144b788400960231da
chansooo <kcs05008@gmail.com>
2023-11-29 17:35:14 +0900

    fix: ProfileImageView 디자인 수정

--- EmotionChatBot/Feature/ChatList/Sources/ChatList/Presenter/ProfileImageView.swift
+++ EmotionChatBot/Feature/ChatList/Sources/ChatList/Presenter/ProfileImageView.swift
@@ -10,45 +10,49 @@ import UIKit
 import RxSwift
 import Kingfisher
 
-final class ProfileImageView: UIImageView {
+public final class ProfileImageView: UIImageView {
     
     // MARK: - UI
     
     // MARK: - Properties
     
     // MARK: - Initializer
-    init() {
+    public init() {
         super.init(frame: .zero)
         
         self.clipsToBounds = true
         self.contentMode = .scaleAspectFill
-        self.backgroundColor = .gray
+//        self.backgroundColor = .gray
+//        self.backgroundColor = UIColor(red: 88, green: 95, blue: 240, alpha: 1)
+        // background color를 #585FF0로 설정
+        self.backgroundColor = UIColor(red: 88/255, green: 95/255, blue: 240/255, alpha: 1)
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Methods
-    override func layoutSubviews() {
+    public override func layoutSubviews() {
         super.layoutSubviews()
         
         let cornerRadius = self.frame.height / 2
         self.layer.cornerRadius = cornerRadius
     }
     
-    func setImage(at profileImageURL: URL?) {
+    public func setImage(at profileImageURL: URL?) {
         let maxProfileImageSize = CGSize(width: 80, height: 80)
         let downsamplingProcessor = DownsamplingImageProcessor(size: maxProfileImageSize)
-        self.kf.setImage(with: profileImageURL, options: [.processor(downsamplingProcessor)])
+        // asset에 있는 robot을 기본 이미지로 넣어준다.
+        self.kf.setImage(with: profileImageURL, placeholder: UIImage(named: "robot"), options: [.processor(downsamplingProcessor)])
     }
 }
 
-extension Reactive where Base: ProfileImageView {
+public extension Reactive where Base: ProfileImageView {
     var setImage: Binder<URL?> {
         return Binder(self.base) { imageView, url in
             let downsamplingProcessor = DownsamplingImageProcessor(size: imageView.frame.size)
-            imageView.kf.setImage(with: url, options: [.processor(downsamplingProcessor)])
+            imageView.kf.setImage(with: url, placeholder: UIImage(named: "robot"),options: [.processor(downsamplingProcessor)])
         }
     }
 }
